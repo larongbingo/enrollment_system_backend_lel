35ffcfc63e1e0bc34f87d3f8f272449893b48843
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-10-10 20:37:16 +0800

    Added the registration API

--- src/routes/apis/account.api.router.ts
+++ src/routes/apis/account.api.router.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express';
 
+import { RegisterAPIValidator } from '@validators/account.api.router.validators';
+import { RequestErrorHandlerCreator } from '@lib/request.error.handler';
+import { FailedResponse, SuccessfulResponse } from '@lib/responses'; 
+import { ValidationErrorCheck } from '@lib/validation.error.check';
+import { User } from '@database';
+
 export const Account_API_Router: Router = Router();
 
 /**
@@ -7,7 +13,45 @@ export const Account_API_Router: Router = Router();
  */
 Account_API_Router
 .route('/register')
-.put(function(req, res) {
+.put(function RegistrationRoute(req, res) {
+  
+  RegisterAPIValidator(req);
+
+  if(ValidationErrorCheck(req, res)) {
+    return;
+  }
+
+  (async function AsyncRegistrationRoute() {
+    
+    return User.IsUsernameUnique(req.body.username)
+    .then(async result => {
+
+      // Check if the username is not taken
+      if(!result) {
+        res.json(new FailedResponse([{message: "Username already taken"}]));
+        return;
+      }
+
+      
+      return User.IsEmailUnique(req.body.email)
+      .then(async email => {
+        
+        // Check if the email is not taken
+        if(!email) {
+          res.json(new FailedResponse([{message: "Email already taken"}]));
+          return;
+        }
+        
+        return User.create(req.body)
+        .then(user => {
+          res.json(new SuccessfulResponse({ID: user.id}));
+        });
+      });
+
+    })
+    .catch(RequestErrorHandlerCreator(req, res))
+
+  })();
 
 });
 
@@ -19,7 +63,8 @@ Account_API_Router
 Account_API_Router
 .route('/login')
 .post(function(req, res) {
-
+  req.check('username', 'Username is empty').isLength({min: 1});
+  req.check('password', 'Password is empty').isLength({min: 1});
 });
 
 /**
@@ -28,10 +73,8 @@ Account_API_Router
 Account_API_Router
 .route('/update')
 .post(function(req, res) {
-
+  req.check('newData', 'Invalid JSON').isJSON();
 });
-
-
 
 /**
  * Delete an account/user
