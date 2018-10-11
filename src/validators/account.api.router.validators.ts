f5cf6603df7df17b1d5b6d570542db2f7615ce11
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-10-11 10:50:00 +0800

    Updated min length with not empty

--- src/validators/account.api.router.validators.ts
+++ src/validators/account.api.router.validators.ts
@@ -1,15 +1,17 @@
 import { Request } from 'express';
 
 export function RegisterAPIValidator(req: Request) {
-  req.check('firstName', 'First Name is empty').isLength({min: 1});
-  req.check('middleName', 'Middle Name is empty').isLength({min: 1});
-  req.check('lastName', 'Last Name is empty').isLength({min: 1});
-  req.check('guardian', 'Guardian is empty').isLength({min: 1});
-  req.check('address', 'Address is empty').isLength({min: 1});
-  req.check('email', 'Email is empty').isLength({min: 1});
-  req.check('phoneNumber', 'Phone Number is empty').isLength({min: 1});
-  req.check('username', 'Username is empty').isLength({min: 1});  
-  req.check('confirmPassword', 'Confirm Password is empty').isLength({min: 1});
-  req.check('password', 'Password is empty').isLength({min: 1}).equals(req.body.confirmPassword);
+  req.check('firstName', 'First Name is empty').notEmpty();
+  req.check('middleName', 'Middle Name is empty').notEmpty();
+  req.check('lastName', 'Last Name is empty').notEmpty();
+  req.check('guardian', 'Guardian is empty').notEmpty();
+  req.check('address', 'Address is empty').notEmpty();
+  req.check('email', 'Email is empty').notEmpty();
+  req.check('phoneNumber', 'Phone Number is empty').notEmpty();
+  req.check('username', 'Username is empty').notEmpty();  
+  req.check('confirmPassword', 'Confirm Password is empty').notEmpty();
+  req.check('password', 'Password is empty').notEmpty();
+
+  req.check('password', 'Password does not match with Confirm Password').equals(req.body.confirmPassword);
   req.check('email', 'Email is not valid').isEmail();
 }
