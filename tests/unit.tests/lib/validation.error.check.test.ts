d3fcda2b334cc92ee119806e02b9df373abe7626
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-10-12 11:59:35 +0800

    Added ValidationErrorCheck Test

---
+++ tests/unit.tests/lib/validation.error.check.test.ts
@@ -0,0 +1,17 @@
+import 'mocha';
+
+export function ValidationErrorCheckTest() {
+
+  describe('ValidationErrorCheck Test', function() {
+
+    it('should return true if there are any errors found');
+
+
+    it('should run the res.json if there are any errors');
+
+
+    it('should return false if there are no errors found');
+
+  });
+  
+}
